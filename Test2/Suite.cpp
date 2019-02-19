426fd39d99c07b146e3483663583de5e678e4f96
Lin Wee Kuan Malika <malika.lin-wee-kuan@etu.umontpellier.fr>
2019-02-19 16:04:42 +0100

    affichage de point vert sur fond noir

---
+++ Test2/Suite.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char** argv){
+
+  
+
+
+
+  
+  float r;
+  cin>>r;
+  float i;
+  cin>>i;
+  std::cout<<"le nombre complexe entré est: "<<r<<"+i("<<i<<")"<<endl;
+
+
+  glutInit(&argc,argv); 
+  glutInitWindowSize(800,800); //taille fenetre
+  glutInitDisplayMode(GLUT_RGB); 	// On travaille en RGB
+  int win = glutCreateWindow("Fractale "); //nomme la fenêtre
+
+  float xmin=-2;
+  float xmax=2;
+  float ymin=-2;
+  float ymax=2;
+  
+  float tailleX=xmax-xmin;
+  float tailleY=ymax-ymin;
+  
+  gluOrtho2D(xmin,xmax,ymin,ymax);	      	//zoom du repère
+
+
+  glPointSize(3);
+
+
+  
+  
+  double ZnR=0;
+  double ZnI=0;
+
+  double temporaire;
+  
+  int cmp=0;
+  while(sqrt(pow(ZnR,2)+pow(ZnI,2))<2 && cmp<50){
+
+    temporaire=ZnR;
+    
+    ZnR=pow(ZnR,2)-pow(ZnI,2)+r;    
+    ZnI=2*temporaire*ZnI+i;
+    cmp++;
+    cout<<" ZnR ="<<ZnR<<" ZnI ="<<ZnI <<endl;
+
+
+    glBegin(GL_POINTS); 	//mode affichage de points
+
+     glColor3f(0, 1, 0);
+     glVertex2f(ZnR,ZnI);
+
+     glEnd(); 		       	// Fermer le polygone
+     glFlush(); 
+  }
+
+  if(cmp<50){
+    cout<<"la fonction diverge a partir de n= "<<cmp<<endl;
+    
+  }
+  else
+    cout<<"la fonction converge"<<endl;
+
+
+
+  
+  glutMainLoop(); //permet un "arret sur image"
+}
