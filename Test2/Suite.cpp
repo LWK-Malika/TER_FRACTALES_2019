c04eb6059b12d08a6530e7f3dea1e63a5651b5bb
Lin Wee Kuan Malika <malika.lin-wee-kuan@etu.umontpellier.fr>
2019-02-20 09:47:45 +0100

    petit changement

--- Test2/Suite.cpp
+++ Test2/Suite.cpp
@@ -32,16 +32,17 @@ int main(int argc, char** argv){
   gluOrtho2D(xmin,xmax,ymin,ymax);	      	//zoom du repère
 
 
-  glBegin(GL_LINES);
+  glBegin(GL_LINES); // création du repère
   glColor3f(1, 1,1);
   glVertex2f(-2,0);
-  glVertex2f(2,0);
+  glVertex2f(2,0); 
 
-    glVertex2f(0,-2);
+  glVertex2f(0,-2);
   glVertex2f(0,2);
 
   glEnd();
   glFlush();
+  
   glPointSize(3);
 
 
@@ -53,7 +54,7 @@ int main(int argc, char** argv){
 
   double temporaire;
   
-  int cmp=0;
+  float cmp=0;
   while(sqrt(pow(ZnR,2)+pow(ZnI,2))<2 && cmp<50){
     
     temporaire=ZnR;
@@ -66,7 +67,7 @@ int main(int argc, char** argv){
     
     glBegin(GL_POINTS); 	//mode affichage de points
     
-    glColor3f(0.6, 0, 003);
+    glColor3f(0.6, 0, 3);
     glVertex2f(ZnR,ZnI);
     
     glEnd(); 		        
