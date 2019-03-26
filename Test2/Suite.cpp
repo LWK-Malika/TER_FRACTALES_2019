f67baf9597f26a2caec18344d4c4d0d5fcb3aaf0
Canton Condes Alexandre <alexandre.canton-condes@etu.umontpellier.fr>
2019-03-26 11:57:06 +0100

    Animation plus rapide et enchaine à la suite suivante quand celle-ci converge trop et n'a plus rien d'intéressant à afficher

--- Test2/Suite.cpp
+++ Test2/Suite.cpp
@@ -5,9 +5,18 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+
+//#include <stdio.h>
+//#include <string.h>
+
 using namespace std;
 
 bool pause = false;
+
+struct Complexe {
+  double r;
+  double i;
+}; typedef struct Complexe Complexe;
 
 void clavier(unsigned char key, int x, int y)  // glutKeyboardFunc(clavier);
 {
@@ -20,27 +29,53 @@ void clavier(unsigned char key, int x, int y)  // glutKeyboardFunc(clavier);
   }
 }
 
+/* Créer une fonction qui affiche une suite aux coordonnée du clique souris
+
+void clique (int button, int state, int x, int y) {
+
+
+      cout << "coordonnées en pixel:" << x << ", " << y << endl
+	   << "coordonnée en par rapport a l'axe " << (tailleX / 800) * x + xmin
+	   << "; " << (tailleY / 800) * y + ymin << endl;
+
+      xmin1=(tailleX/800)*x+xmin;
+      ymin1=((tailleY/800)*y+ymin);
+    }
+  glutMouseFunc(clique);
+
+  */
+
 void diverge(double r, double i, int a) 
 {
   double ZnR = 0;
+  double tmp_ZnR;
   double ZnI = 0;
+  double tmp_ZnI;
   double temporaire;
   float cmp = 0;
   clock_t depart = clock();
 
   // glBegin(GL_LINE_STRIP); // mode affichage de lignes
-
-
-  while ((sqrt(pow(ZnR, 2) + pow(ZnI, 2)) < 2) and (cmp < 120)) {
+  
+  while ((sqrt(pow(ZnR, 2) + pow(ZnI, 2)) < 2) and (cmp < 110)) {
 
     // glutKeyboardFunc(clavier);   
 
-    if (!pause && difftime(clock(), depart) >= 50000) {
+    if (!pause && difftime(clock(), depart) >= 30000) {
       glBegin(GL_POINTS); // mode affichage de points  
       temporaire = ZnR;
     
+      tmp_ZnR = ZnR;
+      tmp_ZnI = ZnI;
+
       ZnR = pow(ZnR, 2) - pow(ZnI, 2) + r;    
       ZnI = 2 * temporaire * ZnI + i;
+
+      if (abs(tmp_ZnR - ZnR) < 0.000001 && abs(tmp_ZnI - ZnI) < 0.000001) {
+        cout << "rentre dans la boucle d'arrêt" << endl;
+        cmp = 110;
+      }
+
       cmp++;
       cout << " ZnR =" << ZnR << " ZnI =" << ZnI << endl;
    
@@ -64,12 +99,18 @@ void diverge(double r, double i, int a)
 
 int main(int argc, char** argv) 
 {
- 
+  if (argc != 3) { // Le programme s'arrête si on ne rentre pas le bon nombre de paramètres
+    cerr << " Nombre de paramètre incorrect." << endl;
+    cout << " Rentrez par exemple : ./main 0.1 0.5" << endl;
+    return -1;
+  }
+
   int a; 
   int win;
 
-  float r; 
-  float i; 
+  Complexe c; 
+  c.r = atof(argv[1]);
+  c.i = atof(argv[2]);
 
   float xmin = -2;
   float xmax = 2;
@@ -79,10 +120,10 @@ int main(int argc, char** argv)
   float tailleX = xmax - xmin;
   float tailleY = ymax - ymin;
 
-  cout << "Entrez le nombre complexe : ";
-  cin >> r;
-  cin >> i;
-  cout << "Le nombre complexe entré est : " << r << " + i" << i << "" << endl;
+  // cout << "Entrez le nombre complexe : ";
+  // cin >> r;
+  // cin >> i;
+  cout << "Le nombre complexe entré est : " << c.r << " + i" << c.i << "" << endl;
   cout << "Voulez-vous aussi afficher les suites préféfinies ?" << endl 
     << "Tapez '1' pour oui et '0' pour non : ";
   cin >> a;
@@ -117,12 +158,13 @@ int main(int argc, char** argv)
   glRasterPos2f(-0.05, 1);
   glutBitmapCharacter(GLUT_BITMAP_8_BY_13, '1');
 
-  diverge(r, i, 1);
+  diverge(c.r, c.i, 1);
 
   // Suites prédéfinies
-  double tab[6][2] = {  
+  double tab[7][2] = {  
     {0, 0.6},
 		{0.3,0.5},
+    {0.2, 0.5},
 		{-0.8,0.156},
 		{0.577,0.478},
 		{-0.7436,0.661746},
