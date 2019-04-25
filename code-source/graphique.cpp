27bcd1cefd9f68fcb3f2ec348c3a09b085cc19ff
Bardin Melvin <melvin.bardin@etu.umontpellier.fr>
2019-04-25 18:04:35 +0200

    ajout ligne commentaire

--- code-source/graphique.cpp
+++ code-source/graphique.cpp
@@ -29,32 +29,51 @@ int main(int argc, char* argv[])
 {
   time_t start, stop;
   start = clock();
-  
+
+
+  //initialisation de opengl
   glutInit(&argc, argv); 
   glutInitWindowSize(800, 800); // taille fenetre
   glutInitDisplayMode(GLUT_RGB); // On travaille en RGB
 
+  //créastion de la fenetre
   int win = glutCreateWindow("Fractale de Mandelbrot"); // nomme la fenêtre
   
   glutIgnoreKeyRepeat(1); // ignore la répétition d'évênement clavier (touche enfoncé)
 
+  //appelle de la fonction resetRepere qui met le repère par défaut
   cadre.resetRepere();
 
-  glutDisplayFunc(Rafraichir); // Callback de la fenêtre
+  // evenement de modification de fenetrage, appelle Rafraichir
+  glutDisplayFunc(Rafraichir);
+
+  //evenement clique souris/mollette, appelle la fonction clique
   glutMouseFunc(clique);
+
+  //evenement tirer-glisser, appelle la fonction carre
   glutMotionFunc(carre);
+
+  //evenement clavier
   glutKeyboardUpFunc(clavier);
+  //evenement clavier (touche special: touche directionnelle)
   glutSpecialFunc(touche);
 
-  stop = clock(); 
+  stop = clock();
+  //permet d'avoir une estime du temps d'execution du programme
   cout << "Durée de l'affichage de la fractale : " << difftime(stop, start) << " millisecondes"
     << endl << " soit " << difftime(stop, start) / 10000 << " secondes." << endl << endl;
 
+  //initialise la deuxieme fenetre
+  //les fonction evenement qui suivent concernerons uniquement cette fenetre
   int suite = glutCreateWindow("suite de Mandelbrot"); // nomme la fenêtre
 
+  //initialise la fenetre
   initialise();
-  
+
+  //evenement touche clavier
   glutKeyboardUpFunc(clavierS);
-  glutMouseFunc(cliqueS); 
+  //evenement souris
+  glutMouseFunc(cliqueS);
+  
   glutMainLoop(); // Permet un "arret sur image"
 }
