e16c4c4b0639b93a6c8088807eeb4a32bc42faaf
Alexandre Canton Condes <alexandre.canton-condes@etu.umontpellier.fr>
2019-04-26 11:23:30 +0200

    Nettoyage de graphique.cpp

--- code-source/graphique.cpp
+++ code-source/graphique.cpp
@@ -6,12 +6,14 @@
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+
 #include <ctime> // Pour faire des calculs temporels
 #include <vector>
 #include <cmath>
 #include <iostream>
 using namespace std;
 
+// Classes et variables globales
 rectangle cadre(-2.15, -1.3, 0.55, 1.3); // Contient les dimensions du repère pour Mandelbrot et Julia
 rectangle zoom(-2.15, -1.3, 0.55, 1.3); // Contient les futurs dimensions lors du zoom "tirer-glisser"
 rectangle zoomTmp(0, 0, 0, 0); // Contient les dimensions du zoom à l'instant T-1 
@@ -30,50 +32,42 @@ int main(int argc, char* argv[])
   time_t start, stop;
   start = clock();
 
-
-  //initialisation de opengl
+  // Initialisation de Opengl
   glutInit(&argc, argv); 
   glutInitWindowSize(800, 800); // taille fenetre
   glutInitDisplayMode(GLUT_RGB); // On travaille en RGB
 
-  //création de la fenêtre
-  int win = glutCreateWindow("Fractale de Mandelbrot"); // nomme la fenêtre
+  // Création de la fenêtre
+  int win = glutCreateWindow("Fractale de Mandelbrot"); // Nomme la fenêtre
   
-  glutIgnoreKeyRepeat(1); // ignore la répétition d'évènement clavier (touche enfoncée)
+  glutIgnoreKeyRepeat(1); // Ignore la répétition d'évènement clavier (touche enfoncée)
 
-  //appelle de la fonction resetRepere qui met le repère par défaut
-  cadre.resetRepere();
+  cadre.resetRepere(); // Appelle de la fonction resetRepere qui met le repère par défaut
 
-  // evenement de modification de fenetrage, appelle Rafraichir
-  glutDisplayFunc(Rafraichir);
+  glutDisplayFunc(Rafraichir);  // Evènement de modification de fenêtrage, appelle Rafraichir
 
-  //evenement clique souris/mollette, appelle la fonction clique
-  glutMouseFunc(clique);
+  glutMouseFunc(clique);  // Evènement clique souris/mollette, appelle la fonction clique
 
-  //evenement tirer-glisser, appelle la fonction carre
-  glutMotionFunc(carre);
+  glutMotionFunc(carre);  // Evènement tirer-glisser, appelle la fonction carre
 
-  //evenement clavier
-  glutKeyboardUpFunc(clavier);
-  //evenement clavier (touche special: touche directionnelle)
-  glutSpecialFunc(touche);
+  glutKeyboardUpFunc(clavier);  // Evènement clavier
 
-  stop = clock();
-  //permet d'avoir une estime du temps d'execution du programme
+  glutSpecialFunc(touche);  // Evènement clavier (touche special: touche directionnelle)
+
+  // Estimation du temps d'execution du programme
+  stop = clock();  
   cout << "Durée de l'affichage de la fractale : " << difftime(stop, start) << " millisecondes"
     << endl << " soit " << difftime(stop, start) / 10000 << " secondes." << endl << endl;
 
-  //initialise la deuxieme fenetre
-  //les fonctions evenement qui suivent concernerons uniquement cette fenetre
-  int suite = glutCreateWindow("suite de Mandelbrot"); // nomme la fenêtre
+  // Initialise la deuxième fenêtre
+  // (Les fonctions évènement qui suive concernerons uniquement cette fenêtre)
+  int suite = glutCreateWindow("suite de Mandelbrot"); // Nomme la fenêtre
 
-  //initialise la fenetre
-  initialise();
+  initialise(); // Initialise la fenêtre
 
-  //evenement touche clavier
-  glutKeyboardUpFunc(clavierS);
-  //evenement souris
-  glutMouseFunc(cliqueS);
+  glutKeyboardUpFunc(clavierS); // Evènement touche clavier
+
+  glutMouseFunc(cliqueS);  // Evènement souris
   
   glutMainLoop(); // Permet un "arret sur image"
 }
